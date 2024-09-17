98c8d7624aa6e016cfb0f8cc5e4e9f0f6b50cde9
Eric DiMarzio <127116933+EricDiMarzio@users.noreply.github.com>
2024-09-17 16:59:15 -0500

    auth added with password hashing

--- server/server.js
+++ server/server.js
@@ -3,20 +3,20 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const {checkDatabaseConnection} = require('./models/database');
-
+const userRoutes = require('./routes/userRoutes');
 const PORT = process.env.PORT || 8080;
 const app = express();
-
-app.use(cors());
-app.use(express.json());
+const bcrypt = require('bcrypt');
 
 checkDatabaseConnection();
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello')
 });
 
-// app.use('/api/auth', )
+app.use('/api/auth', userRoutes);
 
 app.use((req, res) => {
   res.status(404).send('Page not found');
