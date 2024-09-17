8cbe0cdae72f6dd584137a1404f2cc0975c87bf8
Peter Larcheveque <larchevequepeter@gmail.com>
2024-09-17 09:08:56 -0700

    added react-redux to client dependencies

--- client/src/pages/Results.jsx
+++ client/src/pages/Results.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import ResultCard from '../components/ResultCard';
 import { useState } from 'react';
 // import { useSelector, useDispatch } from 'react-redux';
