c0cf9bbf2e74d6b7f5d355a9df4f7ab84b2fd417
Dylan Kinsella <dylankinsella@Dylans-MacBook-Pro.local>
2024-09-17 17:05:03 -0600

    removes depreceated amadeus code and files

--- server/utility/airlinkLinkBuilder.js
+++
@@ -1,5 +0,0 @@
-const buildAirlineLink = (origin, destination, departureDate, returnDate) => {
-  return `https://www.kayak.com/flights/${origin}-${destination}/${departureDate}/${returnDate}`;
-};
-
-module.exports = { buildAirlineLink };
