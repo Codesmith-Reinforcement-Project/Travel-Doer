b56f9f2cf10fda2654f08f5228dfeb1596f4f92c
Peter Larcheveque <larchevequepeter@gmail.com>
2024-09-18 12:10:41 -0700

    added styling to homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,18 +3,18 @@ import SearchBar from './../components/SearchBar';
 
 const homepage = () => {
   return (
-    <div>
+    <div className = 'homepage-container'>
       <SearchBar/>
       This is the homepage!
         
-      <div className = 'homepage-container'>
+      {/* <div className = 'homepage-container'> */}
         <h1 className = 'home-div'> Véntûre
         </h1>
         <h3 className = 'home-div'>
             Quality Traveling on a Budget
         </h3>
-        <img className = 'homepage_img' src = '../assets/homepage.jpeg'></img>
-      </div>
+        {/* <img className = 'homepage_img' src = '../assets/homepage.jpeg'></img> */}
+      {/* </div> */}
     </div>
   );
 };
