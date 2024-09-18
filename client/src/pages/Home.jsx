a19ab3b97aed7c71bc7ba2f4051010d614242385
Peter Larcheveque <larchevequepeter@gmail.com>
2024-09-18 11:33:02 -0700

    completed implementation of sign up and login

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 const homepage = () => {
   return (
-    <div>
+    <div className = 'homepage-container'>
         <h1 className = 'home-div'> Véntûre
         </h1>
         <h3 className = 'home-div'>
             Quality Traveling on a Budget
         </h3>
+        <img className = 'homepage_img' src = '../assets/homepage.jpeg'></img>
     </div>
   );
 };
